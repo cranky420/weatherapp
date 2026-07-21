df0d9a7b43c8cd5e40c3bfff8ac9506205215d39
BRSD <brsinghdeo@gmail.com>
2026-07-21 18:39:28 +0530

    added tfn mdules and testretes

--- infra/terraform/environments/dev/.terraform.lock.hcl
+++ infra/terraform/environments/dev/.terraform.lock.hcl
@@ -2,8 +2,10 @@
 # Manual edits may be lost in future updates.
 
 provider "registry.terraform.io/hashicorp/aws" {
-  version = "6.55.0"
+  version     = "6.55.0"
+  constraints = "~> 6.0"
   hashes = [
+    "h1:Ig9l7sGjaNiLk53QHRVF77lnXUGiW0UvZ+Wy7+DQOHw=",
     "h1:QIQoZv637BE40vg7/0gf/5gQbY/ZTngSuJWDl7fJr2s=",
     "zh:1161fb2d032ad982587b2662a5229e5d06598c5b7fc5c86b2ad64d49225047cd",
     "zh:1f412b09bbece216da0ba08106f3bbb42d8c8971c02d032ab518629915086966",
--- infra/terraform/environments/dev/main.tf
+++ infra/terraform/environments/dev/main.tf
@@ -21,3 +21,73 @@ module "networking" {
     "ap-south-1b"
   ]
 }
+module "security" {
+  source = "../../modules/security"
+
+  project_name = var.project_name
+  environment  = var.environment
+
+  vpc_id = module.networking.vpc_id
+}
+module "iam" {
+
+  source = "../../modules/iam"
+
+  project_name = var.project_name
+  environment  = var.environment
+}
+module "ecr" {
+
+  source = "../../modules/ecr"
+
+  project_name = var.project_name
+  environment  = var.environment
+}
+module "cloudwatch" {
+
+  source = "../../modules/cloudwatch"
+
+  project_name = var.project_name
+  environment  = var.environment
+
+  log_retention_days = 30
+}
+module "alb" {
+
+  source = "../../modules/alb"
+
+  project_name = var.project_name
+  environment  = var.environment
+
+  vpc_id = module.networking.vpc_id
+
+  public_subnet_ids = module.networking.public_subnet_ids
+
+  alb_security_group_id = module.security.alb_security_group_id
+}
+module "ecs" {
+
+  source = "../../modules/ecs"
+
+  project_name = var.project_name
+  environment  = var.environment
+
+  aws_region = var.aws_region
+
+  private_subnet_ids = module.networking.private_subnet_ids
+
+  frontend_security_group_id = module.security.frontend_security_group_id
+  backend_security_group_id  = module.security.backend_security_group_id
+
+  execution_role_arn = module.iam.execution_role_arn
+  task_role_arn      = module.iam.task_role_arn
+
+  frontend_repository_url = module.ecr.frontend_repository_url
+  backend_repository_url  = module.ecr.backend_repository_url
+
+  frontend_log_group_name = module.cloudwatch.frontend_log_group_name
+  backend_log_group_name  = module.cloudwatch.backend_log_group_name
+
+  frontend_target_group_arn = module.alb.frontend_target_group_arn
+  backend_target_group_arn  = module.alb.backend_target_group_arn
+}
